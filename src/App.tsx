3e331c77118aa1aa6ead8fa310738953aa6aeb34
Tiago Pinheiro <tiagoliver@protonmail.com>
2024-10-13 19:13:40 +0200

    [Feat] Added tanStack provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import {Navigator} from '@navigation/Navigation';
 import {NavigationContainer} from '@react-navigation/native';
-import 'react-native-gesture-handler';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {PaperProvider} from 'react-native-paper';
+import 'react-native-gesture-handler';
+import {ThemeContentProvider} from '@presentation/context/ThemeContext/ThemeContext';
+
+const queryClient = new QueryClient();
 
 export const App = () => {
   return (
-    <PaperProvider>
-      <NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <ThemeContentProvider>
         <Navigator />
-      </NavigationContainer>
-    </PaperProvider>
+      </ThemeContentProvider>
+    </QueryClientProvider>
   );
 };
