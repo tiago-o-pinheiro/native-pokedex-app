541753afefdd0a65863c8cbbfee2035d771cfdbb
Tiago Pinheiro <tiagoliver@protonmail.com>
2024-10-21 20:27:33 +0200

    [Refactor] Added new helper functions

--- src/config/helpers/formatter.ts
+++ src/config/helpers/formatter.ts
@@ -1,15 +1,19 @@
 export class Formatter {
-  public static currency(value: number): string {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'USD',
-    }).format(value);
+  static capitalize(str: string, allWords: boolean = false) {
+    if (allWords) {
+      return str.replace(/\b\w/g, l => l.toUpperCase());
+    } else {
+      return str.replace(/\b\w/, l => l.toUpperCase());
+    }
   }
-
   public static date(value?: Date): string {
     if (!value) {
       return '';
     }
     return new Intl.DateTimeFormat('en-US').format(value);
+  }
+
+  public static formatText(value: string): string {
+    return value.replace(/[\n\f]/g, ' ');
   }
 }
