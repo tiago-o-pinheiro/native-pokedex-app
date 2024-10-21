aee4867ac7b99477c6235113d251f3c674bbd54a
Tiago Pinheiro <tiagoliver@protonmail.com>
2024-10-21 20:27:55 +0200

    [Feat] Added new entities for mapping

--- src/domain/entities/pokemons.ts
+++ src/domain/entities/pokemons.ts
@@ -1,3 +1,8 @@
+import {Move, Stat} from '@infrasctructure/interfaces/poke-api.responses';
+import {PokemonSpecies} from './pokemon-species';
+import {PokemonEvolution} from './pokemon-evolution';
+import {PokemonMoves} from './pokemon-moves';
+
 export interface PokemonType {
   name: string;
   url: string;
@@ -9,5 +14,12 @@ export interface Pokemon {
   types: PokemonType[];
   avatar: string;
   sprites: string[];
+  moves: Move[];
   color?: string;
+  baseExperience: number;
+  height: number;
+  stats: Stat[];
+  weight: number;
+  species?: PokemonSpecies;
+  evolution?: PokemonEvolution[];
 }
