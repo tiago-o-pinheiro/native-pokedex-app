3aeeff9ec02acdf3579a5aecf610d598e6e3534e
Tiago Pinheiro <tiagoliver@protonmail.com>
2024-10-24 20:55:39 +0200

    [Refactor] TODO - Fix the evolution chain

--- src/presentation/screens/Pokemon/components/DetailSlider/Evolution.tsx
+++ src/presentation/screens/Pokemon/components/DetailSlider/Evolution.tsx
@@ -3,6 +3,7 @@ import {PokemonEvolution} from '@domain/entities/pokemon-evolution';
 import {FadeInImage} from '@presentation/components';
 import {gaps} from '@theme/gaps';
 import {StyleSheet, useWindowDimensions, View} from 'react-native';
+import {FlatList} from 'react-native-gesture-handler';
 import {Text, useTheme} from 'react-native-paper';
 import Icon from 'react-native-vector-icons/FontAwesome6';
 
@@ -18,6 +19,52 @@ const getIdFromUrl = (url: string): number => {
 const getImageLink = (id: number) => {
   return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`;
 };
+
+// const SPECIAL_POKEMON_EVOLUTIONS = ['eevee'];
+
+// const Evolutions = (evolution: PokemonEvolution[]) => {
+//   //Fix this for special pokemons evolutions
+//   return (
+//     <View>
+//       {evolution.map((item, index) => {
+//         if (SPECIAL_POKEMON_EVOLUTIONS.includes(item.name)) {
+//           return (
+//             <View>
+//               <View style={styles.evolutionItem}>
+//                 <FadeInImage
+//                   uri={getImageLink(getIdFromUrl(item.url))}
+//                   style={{width: 80, height: 80}}
+//                 />
+//                 <Text>{Formatter.capitalize(item.name)}</Text>
+//               </View>
+//             </View>
+//           );
+//         }
+//       })}
+//       <FlatList
+//         data={evolution}
+//         keyExtractor={item => item.name}
+//         horizontal
+//         renderItem={({item, index}) => (
+//           <View style={styles.evolutionContainer}>
+//             <View style={styles.evolutionItem}>
+//               <FadeInImage
+//                 uri={getImageLink(getIdFromUrl(item.url))}
+//                 style={{width: 80, height: 80}}
+//               />
+//               <Text>{Formatter.capitalize(item.name)}</Text>
+//             </View>
+//             {index !== evolution.length - 1 && (
+//               <View style={styles.evolutionArrow}>
+//                 <Icon name="arrow-right" size={20} />
+//               </View>
+//             )}
+//           </View>
+//         )}
+//       />
+//     </View>
+//   );
+// };
 
 export const PokemonEvolutionScreen = ({evolution}: PokemonEvolutionProps) => {
   const {width} = useWindowDimensions();
@@ -64,7 +111,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-around',
     flexDirection: 'row',
-    width: '100%',
+    flexWrap: 'wrap',
+    flexGrow: 1,
   },
   evolutionContainer: {
     display: 'flex',
