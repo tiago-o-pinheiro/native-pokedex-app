37697c9b5941899d4a729cc212ca47d29a6e40d1
Tiago Pinheiro <tiagoliver@protonmail.com>
2024-10-24 20:56:28 +0200

    [Refactor] Removed unnecessary imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import {Navigator} from '@navigation/Navigation';
-import {NavigationContainer} from '@react-navigation/native';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {PaperProvider} from 'react-native-paper';
 import 'react-native-gesture-handler';
 import {ThemeContentProvider} from '@presentation/context/ThemeContext/ThemeContext';
 
